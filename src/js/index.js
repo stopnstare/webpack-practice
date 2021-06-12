e6d3de47b59b9958552db8e9167a28d15ed979d4
Lin Fanny <jenny87082446@gmail.com>
2021-06-13 02:04:35 +0800

    modified

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+//import './index.css';
+import '../scss/index.scss';
+import '../index.html';
+import Add from './tool.js'
+
+//console.log("hihihihi");
+let arr = [1, 2, 3];
+arr.map(item => "a" + item);
+
+console.log(Add(5, 5));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path'); //處理檔案路徑
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('css/[name].css'); //這個css的名稱也是吃entry的key!
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const extractCSS = new MiniCssExtractPlugin('css/[name].css'); //這個css的名稱也是吃entry的key!
 
 //console.log("=>", path.resolve(__dirname, 'dist'));
 //裡面就是js物件的寫法的設定
@@ -12,7 +12,7 @@ module.exports = {
     mode: process.env.NODE_ENV,
     context: path.resolve(__dirname, 'src'),
     entry: {
-        index: './index.js',
+        index: './js/index.js',
         about: './about.js',
     }, //設定進入點是哪支js檔案
     //entry: path.resolve(__dirname, 'src'),
@@ -20,14 +20,60 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'), //這裡可以改變資料夾的名稱
         filename: '[name].js'
     },
+    devServer: {
+        compress: true,
+        port: 3000,
+        stats: {
+            assets: true,
+            cached: false,
+            chunkModules: false,
+            chunkOrigins: false,
+            chunks: false,
+            colors: true,
+            hash: false,
+            modules: false,
+            reasons: false,
+            source: false,
+            version: false,
+            warnings: false
+        },
+    },
+
     //css-loader基本設定
     module: {
         rules: [{
-            test: /\.css$/i,
-            use: extractCSS.extract(["css-loader"]),
-        }, ],
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            { //讓index.html在deploy 後 可以出現在dist裡面，使用政則表達是設定可被搬移的副檔名
+                test: /\.(html|ttf)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]' //分別是[路徑][檔名].[副檔名]
+                    }
+                }, ],
+            },
+            {
+
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                //載入babel-loader
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            }
+        ],
     },
     plugins: [
-        extractCSS
+        new MiniCssExtractPlugin()
     ]
 };
